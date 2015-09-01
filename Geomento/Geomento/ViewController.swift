54e96cafe8a08930c42cac0b6e91e2c3c2398916
littlefacto <Vincent@macbook-pro-de-vincent.home>
2015-09-01 19:31:44 +0200

    Project creation

---
+++ Geomento/Geomento/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geomento
+//
+//  Created by Vincent on 01/09/2015.
+//  Copyright © 2015 Little Facto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
