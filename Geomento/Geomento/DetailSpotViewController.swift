b8a2fa77aa92040fef4c0a9480d93aa991ebb695
littlefacto <Vincent@macbook-pro-de-vincent.home>
2015-09-12 15:57:18 +0200

    Added opening of Map App when users clicks on the map in the detail view

--- Geomento/Geomento/DetailSpotViewController.swift
+++ Geomento/Geomento/DetailSpotViewController.swift
@@ -36,6 +36,24 @@ class DetailSpotViewController: UITableViewController, UITextViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - IBAction
+    
+    @IBAction func mapClicked(sender: UITapGestureRecognizer) {
+        let latitute:CLLocationDegrees =  self.spot!.latitude
+        let longitute:CLLocationDegrees =  self.spot!.longitude
+        
+        let regionDistance:CLLocationDistance = 10000
+        let coordinates = CLLocationCoordinate2DMake(latitute, longitute)
+        let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
+        let options = [
+            MKLaunchOptionsMapCenterKey: NSValue(MKCoordinate: regionSpan.center),
+            MKLaunchOptionsMapSpanKey: NSValue(MKCoordinateSpan: regionSpan.span)
+        ]
+        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.openInMapsWithLaunchOptions(options)
+    }
+    
     // MARK: - UITextViewDelegate
     
     func textViewDidChange(textView: UITextView) {
